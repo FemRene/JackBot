c6c31163be4fcb1e9215901a6c70d578e12caab5
Jack <148626202+peaemer@users.noreply.github.com>
2023-12-19 08:23:57 +0000

    Mathe funktion in Chatbot

--- ChatBot.py
+++ ChatBot.py
@@ -1,51 +1,113 @@
 import random
 
-# zufällige Antworten generieren
-random_antwort = [
-    "Oh, wirklich",
-    "Ich finde du siehst heute toll aus, nur mal so am rande",
-    "Interessant.....",
-    "Ich verstehe.....",
-    "Bist du dir sicher?",
-    "Wenn du das sagst",
-    "Ich glaub das wird mir gerade zu viel",
-    "Das ist sehr kompliziert"
-]
+def input_yes_no(text):
+    y = input(text)
+    if y.upper()=="Y":
+        return True
+    else:
+        return False
 
-# Reaktionen auf bestimmte Schlüsselwörter
-reactions = {
-    "hallo": "Hallo?",
-    "essen": "Ich bin eine Maschine, ich habe keine Geschmacksknospen :C",
-    "aussehen": ["Du bist wunderschön", "Dein Aussehen ist großartig","Du hast schöne Augen","Du slayst","Dein Style ist sehr cool","Du riechst nach Old Spice, mega nice *sniff*","Du wirkst sehr talentiert","Du hast nen coolen Vibe","Big Dick energy","Du bist ein richtiger Schnuggiputz uwu"],
-    "lieblingsfarbe": "Meine Lieblingsfarbe ist Blau und deine?",
-    "uhrzeit": "Bitte hier Uhrzeit Value eingeben",
-    "mathe": "Leider kann ich im moment noch nicht Rechnen, frag doch meinen Ersteller",
-    "findest": ["Gefällt mir !","Das ist echt cool !","Der slayed mega !","Seh gut !"]
-}
+def ma_rechner():
+    try:
+        while True:
+            y = input_yes_no("Willst du den Taschenrechner benutzen (Y/N): ")
+            if not y:
+                break
 
-print("Hallo, ich bin JACK'O'BOT, ich hoffe du hast ein Schönen Tag")
-print("Wenn du mich beenden willst schreib einfach 'bye'")
+            operandie = input("Wie willst du rechnen? (+ - / *): ")
+            if operandie not in ['+', '-', '/', '*']:
+                print("Ungültige Operation!")
+                continue
 
-# Benutzereingabe initialisieren
-userinput = input(str("Über was möchtest du mit mir reden?: "))
+            zahl1 = int(input("Welche Zahl?: "))
+            zahl2 = int(input("Soll mit welcher Zahl berechnet werden?: "))
+            summe = None
 
-while userinput.lower() != "bye":
-    userinput = input("Deine Frage/Antwort?: ")
+            if operandie == '+':
+                summe = zahl1 + zahl2
+                print(str(zahl1) + " + " + str(zahl2) + " = " + str(summe))
+            elif operandie == '-':
+                summe = zahl1 - zahl2
+                print(str(zahl1) + " - " + str(zahl2) + " = " + str(summe))
+            elif operandie == '*':
+                summe = zahl1 * zahl2
+                print(str(zahl1) + " * " + str(zahl2) + " = " + str(summe))
+            elif operandie == '/':
+                if zahl2 != 0:
+                    summe = zahl1 / zahl2
+                    print(str(zahl1) + " / " + str(zahl2) + " = " + str(summe))
+                else:
+                    print("Du kannst nicht durch 0 dividieren.")
 
-    user_woerter = userinput.lower().split()
+    except ValueError as e:
+        print("Nur Zahlen sind erlaubt!")
+    except Exception as e:
+        print("Es ist ein Fehler aufgetreten:", e)
 
-    antwort_gefunden = False
-    for einzel_woerter in user_woerter:
-        if einzel_woerter in reactions:
-            if einzel_woerter == "aussehen":
-                print(random.choice(reactions[einzel_woerter]))
-            elif einzel_woerter == "findest":
-                print(random.choice(reactions[einzel_woerter]))
-            else:
-                print(reactions[einzel_woerter])
-            antwort_gefunden = True
+    except ZeroDivisionError as e:
+        print(e)
+        print("Du kannst nicht durch 0 Dividieren.")
+    except ValueError as e:
+        print(e)
+        print("Nur Zahlen!")
+    except Exception as e:
+        print(e)
+        print("Es scheint so als hättest etwas versucht, was in der Mathematik nicht funktioniert!")
 
-    if not antwort_gefunden:
-        print(random.choice(random_antwort))
+def main():
+    # zufällige Antworten generieren
+    random_antwort = [
+        "Oh, wirklich",
+        "Ich finde du siehst heute toll aus, nur mal so am rande",
+        "Interessant.....",
+        "Ich verstehe.....",
+        "Bist du dir sicher?",
+        "Wenn du das sagst",
+        "Ich glaub das wird mir gerade zu viel",
+        "Das ist sehr kompliziert"
+    ]
 
-print("Ein schönen Tag Wünsch ich dir!")
+    # Reaktionen auf bestimmte Schlüsselwörter
+    reactions = {
+        "hallo": "Hallo?",
+        "essen": "Ich bin eine Maschine, ich habe keine Geschmacksknospen :C",
+        "aussehen": ["Du bist wunderschön", "Dein Aussehen ist großartig","Du hast schöne Augen","Du slayst","Dein Style ist sehr cool","Du riechst nach Old Spice, mega nice *sniff*","Du wirkst sehr talentiert","Du hast nen coolen Vibe","Big Dick energy","Du bist ein richtiger Schnuggiputz uwu"],
+        "lieblingsfarbe": "Meine Lieblingsfarbe ist Blau und deine?",
+        "uhrzeit": "Bitte hier Uhrzeit Value eingeben",
+        "mathe": ma_rechner,
+        "findest": ["Gefällt mir !","Das ist echt cool !","Der slayed mega !","Sehr gut !","finde ich gut","naja könnte besser sein"]
+        
+    }
+
+    print("Hallo, ich bin JACK'O'BOT, ich hoffe du hast ein Schönen Tag")
+    print("Wenn du mich beenden willst schreib einfach 'bye'")
+
+    # Benutzereingabe initialisieren
+    userinput = input(str("Über was möchtest du mit mir reden?: "))
+
+    while userinput.lower() != "bye":
+        userinput = input("Deine Frage/Antwort?: ")
+
+        user_woerter = userinput.lower().split()
+
+        antwort_gefunden = False
+        for einzel_woerter in user_woerter:
+            if einzel_woerter in reactions:
+                if einzel_woerter == "aussehen":
+                    print(random.choice(reactions[einzel_woerter]))
+                elif einzel_woerter == "findest":
+                    print(random.choice(reactions[einzel_woerter]))
+                elif einzel_woerter == "mathe":
+                    reactions[einzel_woerter]()
+                    
+                else:
+                    print(reactions[einzel_woerter])
+                antwort_gefunden = True
+
+        if not antwort_gefunden:
+            print(random.choice(random_antwort))
+
+    print("Ein schönen Tag Wünsch ich dir!")
+
+if __name__ == "__main__":
+    main()
